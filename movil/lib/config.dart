093e324ba408cd3b932f1b8d55c007f499c2b2f0
Misael Urquidez Arredondo <misaelcuentuauwu@gmail.com>
2026-04-06 21:23:57 -0700

    movil con el host

--- movil/lib/config.dart
+++ movil/lib/config.dart
@@ -4,6 +4,11 @@ class Config {
   // static const String baseUrl = 'http://10.0.2.2:8000';
 
   // Dispositivo físico → tu IP local ej. '192.168.1.X'
-  static const String baseUrl = 'http://192.168.1.17:8000';
+  //static const String baseUrl = 'http://192.168.1.17:8000';
+
+  //pc de misa(linux)
   //static const String baseUrl = 'http://192.168.0.5:8000';
+
+  //servidor host
+  static const String baseUrl = 'https://rbe-web-production.up.railway.app';
 }
