147f421b347fcd760ad933db0b4b99eb5bc63a3c
Misael Urquidez Arredondo <misaelcuentuauwu@gmail.com>
2026-04-11 16:56:39 -0700

    para el railway v1

--- movil/lib/config.dart
+++ movil/lib/config.dart
@@ -4,13 +4,13 @@ class Config {
   // static const String baseUrl = 'http://10.0.2.2:8000';
 
   // Dispositivo físico → tu IP local ej. '192.168.1.X'
-  static const String baseUrl = 'http://192.168.1.17:8000';
+  //static const String baseUrl = 'http://192.168.1.17:8000';
 
   //pc fernando
   //static const String baseUrl = 'http://192.168.12.237:8000';
 
   //pc de misa(linux)
-  //static const String baseUrl = 'http://192.168.0.5:8000';
+  static const String baseUrl = 'http://192.168.0.5:8000';
 
   //servidor host
   //static const String baseUrl = 'https://rbe-web-production.up.railway.app';
