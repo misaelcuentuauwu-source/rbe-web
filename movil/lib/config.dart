370a59c46567324f1b373e27fece4f45c07e5dce
Misael Urquidez Arredondo <misaelcuentuauwu@gmail.com>
2026-04-14 14:58:59 -0700

    conexion lan movil

--- movil/lib/config.dart
+++ movil/lib/config.dart
@@ -1,5 +1,6 @@
 class Config {
   // Cambia esto según donde corras el servidor:
+
   // Para emulador Android en PC:
   // static const String baseUrl = 'http://10.0.2.2:8000';
 
@@ -16,7 +17,10 @@ class Config {
   //static const String baseUrl = 'http://192.168.1.13:8000';
 
   //pc de misa(linux)
-  static const String baseUrl = 'http://192.168.137.37:8000';
+  //static const String baseUrl = 'http://192.168.137.37:8000';
+
+  //pc de misa(linux-sin-internet)
+  static const String baseUrl = 'http://10.42.0.1:8000';
 
   //servidor host
   //static const String baseUrl = 'https://rbe-web-production.up.railway.app';
